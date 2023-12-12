83967cbebb51cacdd396d3749486538a2e80a81c
arthurdesma <arthurchamonix@gmail.com>
2023-12-12 15:34:00 +0100

    up

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from BDD import connect_db, insert_data_if_not_exists
 
-
 def fetch_race_links(year):
     url = f"https://www.formula1.com/en/results.html/{year}/races.html"
     response = requests.get(url)
@@ -39,21 +38,17 @@ def year_result(year):
 
     table = soup.find("table", class_="resultsarchive-table")
 
-    # Initialize an empty list to hold the race data
     race_table = []
 
-    # Iterate over each row in the table body
-    for row in table.find_all("tr")[1:]:  # [1:] skips the header row
+    for row in table.find_all("tr")[1:]:
         columns = row.find_all("td")
 
-        # Extract data from the relevant columns
         grand_prix = columns[1].get_text(strip=True)
         winner = columns[3].get_text(strip=True)
         car = columns[4].get_text(strip=True)
         laps = columns[5].get_text(strip=True)
         time = columns[6].get_text(strip=True)
 
-        # Add the data to the race_data list
         race_table.append(
             {
                 "Grand Prix": grand_prix,
@@ -65,31 +60,29 @@ def year_result(year):
             }
         )
 
-    # Now race_data contains all the extracted information
     return(race_table)
 
 
-def race_number(year,race):
-    url = (
-        f"https://www.formula1.com/en/results.html/{year}/races/{race} race-result.html"
-    )
+def race_number(year, race):
+    url = f"https://www.formula1.com/en/results.html/{year}/races/{race}/race-result.html"
     response = requests.get(url)
 
     if response.status_code != 200:
-        return f"Failed to fetch data for year {year}: {response.status_code}"
+        print(f"Failed to fetch data for year {year}: {response.status_code}")
+        return []
 
     soup = BeautifulSoup(response.text, "html.parser")
-
     table = soup.find("table", class_="resultsarchive-table")
 
-    # Initialize an empty list to hold the race data
-    race_table = []
+    # Skip if the table is not found
+    if table is None:
+        print(f"No data found for year {year} and race {race}")
+        return []
 
-    # Iterate over each row in the table body
-    for row in table.find_all("tr")[1:]:  # [1:] skips the header row
+    race_table = []
+    for row in table.find_all("tr")[1:]:
         columns = row.find_all("td")
 
-        # Extract data from the relevant columns
         Pos = columns[1].get_text(strip=True)
         No = columns[2].get_text(strip=True)
         Driver = columns[3].get_text(strip=True)
@@ -97,7 +90,6 @@ def race_number(year,race):
         time = columns[6].get_text(strip=True)
         Pts = columns[7].get_text(strip=True)
 
-        # Add the data to the race_data list
         race_table.append(
             {
                 "Position": Pos,
@@ -111,42 +103,31 @@ def race_number(year,race):
             }
         )
 
-    # Now race_data contains all the extracted information
-    return(race_table)
+    return race_table
 
 
 all_race_data = []
-for year in range(1950, 1951):
+for year in range(2023, 2024):
     race_links = fetch_race_links(year)
-    if isinstance(race_links, list):  # Check if the returned value is a list
+    if isinstance(race_links, list): 
         all_race_data.extend(race_links)
     else:
-        print(race_links)  # Print error message
+        print(race_links)
 
 
-# Now you can process all_race_data as needed
-# for race in all_race_data:
-#     print(race["year"], race["data_value"], race["href"])
-
-# Specify database names
-# Database and collection names
 database_name = 'racing_database'
 collection_name_1 = 'grand_prix_results'
 collection_name_2 = 'driver_standings'
 
-# Connect to collections
 collection_1 = connect_db(database_name, collection_name_1)
 collection_2 = connect_db(database_name, collection_name_2)
 
-# Process data for the first collection
-for year in range(1950, 1951):
+for year in range(2023, 2024):
     year_result_data = year_result(year)
     insert_data_if_not_exists(collection_1, year_result_data, ['Grand Prix', 'Winner','Car','Laps','Time','Years'])
 
-# Process data for the second collection
 for race in all_race_data:
     result_data = race_number(race["year"], race["data_value"])
-    # Specify the fields to check for existing data in collection_2
     insert_data_if_not_exists(collection_2, result_data, ['Numero pilot', 'Driver name','Position','Laps','Time','Points','Grand Prix','Years'])
 
 print("finish")
