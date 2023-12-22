7be54e08f32b7e8ba860f0bc17a011c8a4313a95
arthurdesma <arthurchamonix@gmail.com>
2023-12-22 15:06:38 +0100

    Revert "up"
    This reverts commit 1dd5deaf03cf3b5665730e81d1bb30611f0c6058.

--- scrap.py
+++ scrap.py
@@ -3,111 +3,132 @@ from bs4 import BeautifulSoup
 
 # Function to fetch race links
 def fetch_race_links(year):
-    url = f"https://www.formula1.com/en/results.html/{year}/races.html"
-    response = requests.get(url)
+    try:
+        print(f"Attempting to connect to URL for year {year}")  # Debug message
+        url = f"https://www.formula1.com/en/results.html/{year}/races.html"
+        response = requests.get(url, timeout=10)
 
-    if response.status_code != 200:
-        return f"Failed to fetch data for year {year}: {response.status_code}"
+        print("Connection attempt complete.")  # Debug message
 
-    soup = BeautifulSoup(response.text, "html.parser")
+        if response.status_code != 200:
+            return f"Failed to fetch data for year {year}: HTTP Status Code {response.status_code}"
 
-    items = soup.find_all(
-        lambda tag: tag.name == "a"
-        and "resultsarchive-filter-item-link" in tag.get("class", [])
-        and tag.get("data-name") == "meetingKey"
-    )
+        print(f"Successfully connected to the website for year {year}.")  # Connection successful message
 
-    race_data = []
-    for element in items:
-        data_value = element.get("data-value")
-        if data_value:
-            href = "https://www.formula1.com" + element.get("href")
-            race_data.append({"year": year, "data_value": data_value, "href": href})
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    return race_data
+        print("Parsing HTML...")  # Debug message
+
+        items = soup.find_all(
+            lambda tag: tag.name == "a"
+            and "resultsarchive-filter-item-link" in tag.get("class", [])
+            and tag.get("data-name") == "meetingKey"
+        )
+
+        print("HTML parsing complete. Processing data...")  # Debug message
+
+        race_data = []
+        for element in items:
+            data_value = element.get("data-value")
+            if data_value:
+                href = "https://www.formula1.com" + element.get("href")
+                race_data.append({"year": year, "data_value": data_value, "href": href})
+
+        print("Data processing complete.")  # Debug message
+
+        return race_data
+
+    except requests.exceptions.Timeout:
+        return f"Request timed out for year {year}"
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 
-print(fetch_race_links(2023))
 # Function to get year result
-
 def year_result(year):
-    url = f"https://www.formula1.com/en/results.html/{year}/races.html"
-    response = requests.get(url)
+    try:
+        print(f"Attempting to connect to URL for year {year}...")
+        url = f"https://www.formula1.com/en/results.html/{year}/races.html"
+        response = requests.get(url, timeout=30)
 
-    if response.status_code != 200:
-        return f"Failed to fetch data for year {year}: {response.status_code}"
+        if response.status_code != 200:
+            return f"Failed to fetch data for year {year}: HTTP Status Code {response.status_code}"
 
-    soup = BeautifulSoup(response.text, "html.parser")
+        print(f"Successfully connected for year {year}.")
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    table = soup.find("table", class_="resultsarchive-table")
+        table = soup.find("table", class_="resultsarchive-table")
+        if table is None:
+            return f"No data found for year {year}"
 
-    race_table = []
+        race_table = []
+        for row in table.find_all("tr")[1:]:
+            columns = row.find_all("td")
+            grand_prix = columns[1].get_text(strip=True)
+            winner = columns[3].get_text(strip=True)
+            car = columns[4].get_text(strip=True)
+            laps = columns[5].get_text(strip=True)
+            time = columns[6].get_text(strip=True)
 
-    for row in table.find_all("tr")[1:]:
-        columns = row.find_all("td")
-
-        grand_prix = columns[1].get_text(strip=True)
-        winner = columns[3].get_text(strip=True)
-        car = columns[4].get_text(strip=True)
-        laps = columns[5].get_text(strip=True)
-        time = columns[6].get_text(strip=True)
-
-        race_table.append(
-            {
+            race_table.append({
                 "Grand Prix": grand_prix,
                 "Winner": winner,
                 "Car": car,
                 "Laps": laps,
                 "Time": time,
-                "Years": year,
-            }
-        )
+                "Year": year,
+            })
 
-    return race_table
+        return race_table
 
-# print(year_result(2023))
+    except requests.exceptions.Timeout:
+        return f"Request timed out for year {year}"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+
 
 # Function to get race number result
 def race_number(year, race):
-    url = (
-        f"https://www.formula1.com/en/results.html/{year}/races/{race}/race-result.html"
-    )
-    response = requests.get(url)
+    try:
+        print(f"Attempting to connect to URL for year {year}, race {race}...")
+        url = f"https://www.formula1.com/en/results.html/{year}/races/{race}/race-result.html"
+        response = requests.get(url, timeout=30)
 
-    if response.status_code != 200:
-        print(f"Failed to fetch data for year {year}: {response.status_code}")
-        return []
+        if response.status_code != 200:
+            return f"Failed to fetch data for year {year}, race {race}: HTTP Status Code {response.status_code}"
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    table = soup.find("table", class_="resultsarchive-table")
+        print(f"Successfully connected for year {year}, race {race}.")
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    # Skip if the table is not found
-    if table is None:
-        print(f"No data found for year {year} and race {race}")
-        return []
+        table = soup.find("table", class_="resultsarchive-table")
+        if table is None:
+            return f"No data found for year {year} and race {race}"
 
-    race_table = []
-    for row in table.find_all("tr")[1:]:
-        columns = row.find_all("td")
+        race_table = []
+        for row in table.find_all("tr")[1:]:
+            columns = row.find_all("td")
+            pos = columns[1].get_text(strip=True)
+            no = columns[2].get_text(strip=True)
+            driver = columns[3].get_text(strip=True)
+            laps = columns[5].get_text(strip=True)
+            time = columns[6].get_text(strip=True)
+            pts = columns[7].get_text(strip=True)
 
-        Pos = columns[1].get_text(strip=True)
-        No = columns[2].get_text(strip=True)
-        Driver = columns[3].get_text(strip=True)
-        laps = columns[5].get_text(strip=True)
-        time = columns[6].get_text(strip=True)
-        Pts = columns[7].get_text(strip=True)
-
-        race_table.append(
-            {
-                "Position": Pos,
-                "Numero pilot": No,
-                "Driver name": Driver,
+            race_table.append({
+                "Position": pos,
+                "Numero pilot": no,
+                "Driver name": driver,
                 "Laps": laps,
                 "Time": time,
-                "Points": Pts,
+                "Points": pts,
                 "Grand Prix": race.split("/")[-1],
-                "Years": year,
-            }
-        )
+                "Year": year,
+            })
 
-    return race_table
+        return race_table
+
+    except requests.exceptions.Timeout:
+        return f"Request timed out for year {year}, race {race}"
+    except Exception as e:
+        return f"An error occurred: {e}"
